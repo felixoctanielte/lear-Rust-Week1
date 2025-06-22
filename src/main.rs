58322de5d43749c081846bddc3f0de46ef76b50b
Felix Octaniel <felixoctanieltel05@gmail.com>
2025-06-23 00:24:10 +0700

    learn day1

---
+++ src/main.rs
@@ -0,0 +1,41 @@
+use std::io;
+
+// fn main() {
+//     println!("Guess the number!");
+//     println!("Please input your gues.");
+    
+
+//     let mut guess = String::new();
+
+//     io::stdin() 
+
+//         .read_line( &mut guess)
+//         .expect("Failed to read line");
+
+//     println!("You Guessed:{}", guess); 
+
+
+// } 
+
+
+fn main(){
+    println!("Guess the number!");
+    println!("Please input your number.");
+
+    let mut guess = String::new();
+
+    io::stdin()
+
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+    println!("You guessed: {}", guess);
+
+
+}
+
+
+
+
+
+
+// let s: String = String::from("value");
